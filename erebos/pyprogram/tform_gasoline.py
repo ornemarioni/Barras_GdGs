5a8cdca4cdc10811ed015c31dacad8f40068e566
ornemarioni <ornelamarioni@gmail.com>
2018-09-05 22:59:54 +0200

    erebos: plot terminado, faltaria acomodar el tform_gasoline, para que no se me pierdan las particulas

--- erebos/pyprogram/tform_gasoline.py
+++ erebos/pyprogram/tform_gasoline.py
@@ -12,28 +12,32 @@ path = '/srv/cosmdatc/clues/B64_WM3_186592/LG/GAS_SFR/4096_Gasoline/'
 for l in range(0,3):
     
     file = np.loadtxt(path2 + str('%s'%vector2[l]) +'_tform_z0.dat')
+    # file = np.loadtxt(path2 + 'M33_tform_z0_2.dat')
     ID    = file[:,0]
     tform = file[:,2]
-    
+
     sort = np.argsort(tform)
+
+    sort_tform = tform[sort]
+    sort_ID    = ID[sort]
+
+#     file2 = np.loadtxt(path2 + 'M33_masscenter.dat')
+    file2 = np.loadtxt(path2 + str('%s'%vector2[l]) +'_masscenter.dat')
     
-    sort_tform = tform[sort][::-1]
-    sort_ID    = ID[sort][::-1]
-    
-    file2 = np.loadtxt(path2 +  str('%s'%vector2[l]) +'_masscenter.dat')
     time = file2[:,0]
     xcm  = file2[:,1]
     ycm  = file2[:,2]
     zcm  = file2[:,3]
-    
+
+    time2 = time[::-1]
     
     time_aux = np.zeros(len(ID))
 
-    k = 1
+    k = 0
     for i in range(0,len(ID)):
         for j in range(k, len(time)):
-            if sort_tform[i] > time[j]:
-                time_aux[i] = time[j-1]
+            if sort_tform[i] < time[j]:
+                time_aux[i] = time[j]
     #             print time_aux[i]
                 if time_aux[i] < time_aux[i-1]:
                     k = k + 1
@@ -41,9 +45,12 @@ for l in range(0,3):
 
     snapshot = np.loadtxt('/z/omarioni/snapshots.txt', dtype='string') #SNAPSHOTS
     isnap = snapshot[::-1]
-    
-    archivo = open(path2 + str('%s'%vector2[l]) + '_tform_particles.dat', 'a')
 
+#     archivo = open(path2 + 'M33_tform_particles.dat', 'a')
+    archivo = open(path2 + str('%s'%vector2[l]) +'_tform_particles.dat', 'a')
+    
+    time_aux = time_aux[::-1]
+    
     for i in range(0, len(time)):
 
         mask, = np.where(time_aux == time[i])
@@ -53,8 +60,6 @@ for l in range(0,3):
 
         s = pynbody.load(path + str('%s'%isnap[i])+'/WMAP3.CLUES.HR.00'+ str('%s'%isnap[i]))
 
-        IDs = s.star['iord']
-        
         pstr = s.star['pos'].in_units('kpc')
         IDs  = s.star['iord']
         tf   = s.star['tform'].in_units('Gyr')
@@ -71,9 +76,9 @@ for l in range(0,3):
         data[:,0] = IDs[particles]
         data[:,1] = rstr
         data[:,2] = tf[particles]
-        
-        np.savetxt(archivo, data, fmt=('%15d','%12.6f','%12.6f'))
-        
+
+        np.savetxt(archivo, data, fmt=('%10d','%12.6f','%12.6f'))
+
     archivo.close()
 
             
