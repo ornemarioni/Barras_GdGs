4975c9a7b8f8eab7c8e56a750b6c1f37c0845052
ornemarioni <ornemarioni@hotmail.com>
2019-10-21 02:24:26 -0300

    calculo de A1

--- Barras_Gd/programs/furier_components.py
+++ Barras_Gd/programs/furier_components.py
@@ -1,63 +1,34 @@
 import numpy as np
-import bines
-import math
+import bines2 as bines 
+
+def am(m,x,y,nbin, modo):
+    rcil = np.sqrt(x**2 + y**2)
+    rbin, nodos = bines.rbin1(rcil, nbin)
+    
+    Amv  = np.ndarray(nbin)
+    phiv = np.ndarray(nbin)
+    
+    n = len(x)
+    delta = n / nbin
+    
+    r_sort = np.sort(rcil)
+    
+    for j in range(0, nbin):
+        mask, = np.where((rcil > r_sort[j*(delta-1)]) & (rcil <= r_sort[(delta-1)*(j+1)]))
+        
+        xn = x[mask]
+        yn = y[mask]
+        
+        titaj = np.arctan2(yn,xn)
+        
+        a0 = np.sum(m[mask])
+        am = np.sum(m[mask]*np.cos(modo*titaj))
+        bm = np.sum(m[mask]*np.sin(modo*titaj))
+        
+        Amv[j]  = np.sqrt(am**2 + bm**2) / a0
+        phiv[j] = np.arctan2(bm,am) / 2.
+        
+
+    return A2v, phiv, rbin
 
 
-
-def a2max(m,x,y,nbin):
-
-
-    rcil=np.sqrt(x**2+y**2)
-    rbin=bines.rbin1(rcil,np.ones(np.size(m)),nbin)
-    A2v=np.ndarray(nbin)
-    a2=np.ndarray(nbin)
-    b2=np.ndarray(nbin)
-    phiv=np.ndarray(nbin)
-    out=np.ndarray([2,nbin])
-    for j in range(1,nbin):
-        kk,=np.where((rcil>rbin[j-1]) & (rcil<=rbin[j]))
-        xnk=x[kk]
-        ynk=y[kk]
-        titaj=np.ndarray(np.size(kk))
-        for ii in range(np.size(kk)):
-            titaj[ii]=math.atan2(ynk[ii],xnk[ii])
-
-        rj=np.sqrt(x[kk]**2+y[kk]**2) 
-        a0=sum(m[kk])
-        a2[j]=sum(m[kk]*np.cos(2*titaj))
-        b2[j]=sum(m[kk]*np.sin(2*titaj))
-        A2v[j]=np.sqrt(a2[j]**2+b2[j]**2)/a0
-
-    A2max=max(A2v)
-    kA2=np.argmax(A2v) 
-    rbinmax=rbin[kA2]
-    a2_max=a2[kA2]
-    b2_max=b2[kA2]
-
-    return a2_max, b2_max, rbinmax
-
-def a2(m,x,y,nbin):
-
-    rcil=np.sqrt(x**2+y**2)
-    rbin=bines.rbin1(rcil,np.ones(np.size(m)),nbin)
-    A2v=np.ndarray(nbin)
-    phiv=np.ndarray(nbin)
-    a2=np.ndarray(nbin)
-    b2=np.ndarray(nbin)
-    out=np.ndarray([2,nbin])
-    for j in range(1,nbin):
-        kk,=np.where((rcil>rbin[j-1]) & (rcil<=rbin[j]))
-        xnk=x[kk]
-        ynk=y[kk]
-        titaj=np.ndarray(np.size(kk))
-        for ii in range(np.size(kk)):
-            titaj[ii]=math.atan2(ynk[ii],xnk[ii])
-
-        rj=np.sqrt(x[kk]**2+y[kk]**2) 
-        a0=sum(m[kk])
-        a2[j]=sum(m[kk]*np.cos(2*titaj))
-        b2[j]=sum(m[kk]*np.sin(2*titaj))
-        A2v[j]=np.sqrt(a2[j]**2+b2[j]**2)/a0
-
-
-    return A2v,rbin, a2, b2
